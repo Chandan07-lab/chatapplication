c66fe6db086096a4fbb58a1f5e1eaddff416668a
Chandan07-lab <116368780+Chandan07-lab@users.noreply.github.com>
2025-04-21 18:50:44 +0530

    Add Jenkins pipeline

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -14,3 +14,18 @@ export const generateToken = (userId, res) => {
 
   return token;
 };
+
+// import jwt from "jsonwebtoken";
+
+// export const protectRoute = (req, res, next) => {
+//   const token = req.cookies.jwt;
+//   if (!token) return res.status(401).json({ message: "Unauthorized" });
+
+//   try {
+//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+//     req.user = decoded;
+//     next();
+//   } catch (err) {
+//     res.status(401).json({ message: "Invalid token" });
+//   }
+// };
